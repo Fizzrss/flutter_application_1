718602b25239f182778b3c2484ddfbde689b3acd
Fizzrss <fizars123@gmail.com>
2026-04-08 06:09:49 +0700

    Set State

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;     // ← Add this property.
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,9 +73,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,    // ← Change to this.
               onDestinationSelected: (value) {
-                print('selected: $value');
+
+                // ↓ Replace print with this.
+                setState(() {
+                  selectedIndex = value;
+                });
+
               },
             ),
           ),
