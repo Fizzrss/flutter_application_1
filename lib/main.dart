54bf592c086252ddb673034140e3080fdba7e0dd
Fizzrss <fizars123@gmail.com>
2026-04-07 17:04:39 +0700

    Mengekstrak widget

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var pair = appState.current;   
+    var pair = appState.current; // ← Add this.  
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'), // ← Example change.
-          Text(pair.asLowerCase),
+          Text(pair.asLowerCase), // ← Change to this.
 
           ElevatedButton(
             onPressed: () {
